073c42db456366ad48de99d38c4269cbd793835b
Jie Zhou <chowstat@gmail.com>
2022-08-30 11:43:32 -0400

    ready to do

--- scripts/simulation.R
+++ scripts/simulation.R
@@ -5,7 +5,7 @@ library(lglasso)
 library(BDgraph)
 library(GGMselect)
 #set.seed(1)
-results=power_compare(m=20,n=5,p=80,coe = c(2.5,0,0),l=10,rho = 0.1,prob=0.01,heter=T)
+results=power_compare(m=20,n=10,p=80,coe = c(2,0.5,0.5),l=10,rho = 0.1,prob=0.01,heter=T)
 
 
 
