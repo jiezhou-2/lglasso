5ebd133929a956641fb5f89fce29eb31c872239e
Jie Zhou <chowstat@gmail.com>
2025-08-23 20:52:39 -0400

    added new code for tissue related data

---
+++ R/functionV1.R
@@ -0,0 +1,418 @@
+
+#' Construct the temporal component fo correlation function
+#'
+#' @param t Time points of observations
+#' @param tau length 2 vector representing the correlation parameter between time points
+#' @param type The type of correlation function, which typically take either 0,1 or 2.
+#' @author Jie Zhou
+#' @noRd
+#' @return A square matrix with dimension equal to the length of vector t
+phifunction=function(t,tau){
+  n=length(t)
+  #print(tau)
+  if (any(tau<=0)){stop("tau should be positive!")}
+  if (n==1){
+    M=as.matrix(1)
+  }else{
+    M=matrix(nrow = n, ncol = n)
+    for (i in 1:n) {
+      for (j in i:n){
+        M[i,j]=exp(-tau[1]*(abs(t[i]-t[j])))^tau[2]
+        M[j,i]=M[i,j]
+      }
+    }
+    diag(M)=1
+  }
+  return(M)
+}
+
+
+
+
+AA=function(B,data,type=c("general","expFixed","twoPara"),expFix,maxit=100,
+            tol=10^(-4),lower=c(0.01,0.1),upper=c(10,5),...){
+  ### clustered data
+  type=match.arg(type)
+  if (type=="general"){
+    m3=length(unique(data[,2]))
+    p=ncol(data)-2
+    nn=length(unique(data$subject))
+    subjects=unique(data$subject)
+    if (ncol(B)!=p | nrow(B)!=p){stop("Inputs do not match with each other!")}
+    A=Variable(m3,m3,PSD=T) # tissue wise inverse correlation matrix
+    obj1=(p*nn)/2*log_det(A)
+
+
+
+    data_sub=split(data[,-c(1,2)],data$subject)
+    amatrix=Reduce("+",lapply(data_sub, function(B,xx){as.matrix(xx)%*%B%*%t(as.matrix(xx))}, B=B))
+    obj2=-0.5*matrix_trace(A%*%amatrix)
+    obj=-(obj1+obj2)
+    constr=list(CVXR::diag(A)==1)
+
+
+
+    prob=Problem(Minimize(obj),constr)
+    results=CVXR::solve(prob)
+    corMatrix=solve(results$getValue(A))
+    return(list(corMatrix=corMatrix))
+  }
+
+
+  ### longitudinal data
+
+  if (type == "expFixed"){
+    p=ncol(data)-2
+    nn=length(unique(data$subject))
+    subjects=unique(data$subject)
+    if (ncol(B)!=p | nrow(B)!=p){stop("Inputs do not match with each other!")}
+    #YY=Variable(p,p,PSD=TRUE)
+    time_list=split(data[,2],data[,1])
+    subdata_list=split(data[,-c(1,2)],data$subject)
+
+    likefun=function(tau){
+      obj1=0
+      obj2=0
+      for (i in 1:nn) {
+        datai=as.matrix(subdata_list[[i]])
+        t=time_list[[i]]
+        Aa=phifunction(t=t,tau = c(tau,expFix))
+        amatrix=datai%*%B%*%t(datai)
+        obji1=-0.5*p*log(det(Aa))
+        obji2= -0.5*sum(diag(solve(Aa)%*%amatrix))
+        obj1=obj1+obji1
+        obj2=obj2+obji2
+      }
+
+      obj=-(obj1+obj2)
+    }
+
+    tau=optim(c(control$tau0[1]),likefun,method = "L-BFGS-B",lower = lower,upper = upper)$par
+    A=lapply(time_list,phifunction,tau=c(tau,expFix))
+    return(list(corMatrixList=A,tau=tau))
+  }
+
+
+  ### longitudinal data
+  if (type == "twoPara"){
+    p=ncol(data)-2
+    nn=length(unique(data$subject))
+    subjects=unique(data$subject)
+    if (ncol(B)!=p | nrow(B)!=p){stop("Inputs do not match with each other!")}
+    #YY=Variable(p,p,PSD=TRUE)
+    time_list=split(data[,2],data[,1])
+    subdata_list=split(data[,-c(1,2)],data$subject)
+
+    likefun=function(tau){
+      obj1=0
+      obj2=0
+      for (i in 1:nn) {
+        datai=as.matrix(subdata_list[[i]])
+        t=time_list[[i]]
+        Aa=phifunction(t=t,tau = c(tau,1))
+        amatrix=datai%*%B%*%t(datai)
+        obji1=-0.5*p*log(det(Aa))
+        obji2= -0.5*sum(diag(solve(Aa)%*%amatrix))
+        obj1=obj1+obji1
+        obj2=obj2+obji2
+      }
+
+      obj=-(obj1+obj2)
+    }
+
+    tau=optim(c(control$tau0),likefun,method = "L-BFGS-B",lower = lower,upper = upper)$par
+    A=lapply(time_list,phifunction,tau=tau)
+    return(list(corMatrixList=A,tau=tau))
+  }
+
+}
+
+
+
+
+
+
+
+
+
+BB=function(A,data,lambda,type=c("general","expFixed","twoPara"),maxit=100,
+            tol=10^(-4),lower=c(0.01,0.1),upper=c(10,5),...){
+  type=match.arg(type)
+  if (type=="general"){
+    m3=length(unique(data[,2]))
+    p=ncol(data)-2
+    nn=length(unique(data$subject))
+    subjects=unique(data$subject)
+    if (ncol(A)!=m3 | nrow(A)!=m3){
+      stop("Inputs do not match with each other!")
+    }
+
+    data_sub=split(data[,-c(1,2)],data$subject)
+    amatrix=Reduce("+",lapply(data_sub, function(A,xx){t(as.matrix(xx))%*%solve(A)%*%as.matrix(xx)}, A=A))
+    bb=glasso::glasso(s=amatrix/(m3*nn),rho=lambda[1])$wi
+    return(list(preMatrix=bb))
+  }
+  if (type == "expFixed"){
+    p=ncol(data)-2
+    nn=length(unique(data$subject))
+    subjects=unique(data$subject)
+
+    data_sub=split(data[,-c(1,2)],data$subject)
+    if (length(A)!=length(data_sub)){stop("Data do not match!")}
+    bmate=0
+    for (i in 1:length(A)) {
+      xx=as.matrix(data_sub[[i]])
+      yy=solve(as.matrix(A[[i]]))
+      bmate=t(xx)%*%yy%*%xx+bmate
+    }
+    bb=glasso::glasso(s=bmate/nrow(data),rho = lambda[1])$wi
+
+    return(list(preMatrix=bb))
+
+  }
+
+  if (type == "twoPara"){
+    p=ncol(data)-2
+    nn=length(unique(data$subject))
+    subjects=unique(data$subject)
+
+    data_sub=split(data[,-c(1,2)],data$subject)
+    if (length(A)!=length(data_sub)){stop("Data do not match!")}
+    bmate=0
+    for (i in 1:length(A)) {
+      xx=as.matrix(data_sub[[i]])
+      yy=solve(as.matrix(A[[i]]))
+      bmate=t(xx)%*%yy%*%xx+bmate
+    }
+    bb=glasso::glasso(s=bmate/nrow(data),rho = lambda[1])$wi
+
+    return(list(preMatrix=bb))
+
+  }
+
+}
+
+
+#' @title Longitudinal graphical lasso
+#' @description
+#'  This is the main function of the package, which identifies the underlying  network model from clustered data. Here
+#'  clustered data include longitudinal data, or spatially correlated data,
+#'  e.g, metabolites in different tissues of a same subject.  .
+#'  or more broadly, clustered data for given tuning parameters.
+#'
+#'
+#' @param data a \code{n} by \code{(p+2)} data frame in which the first column is subject ID, the second column is
+#' the time point for longitudinal data or tissue ID.
+#' @param lambda  a vector of length 1 or 2,  which
+#' is the tuning parameter for the identification of the networks. For details, see the explanations in the below.
+#' @param type a string specifying which model need to be fitted. There are three models available,
+#' which are refered as \code{general, expFixed} and \code{twoPara} respectively.
+#'  Please see the details in the below for the meaning of each.
+#' @param expFix a nemeric number used in the model specification
+#' @param group a vector  of length \code{n} if supplied which specify which data
+#'  points need to be grouped together to infer a network for each of them.
+#' @param maxit the maximum iterations for the estimation.
+#' @param tol the minimum value for  convergence criterion
+#' @param lower a vector of length 1 or 2 which specifies the lower bounds for the optimization algorithm
+#' @param upper a vector of length 1 or 2 which specifies the upper bounds for the optimization algorithm
+#' @param ... other inputs
+#' @import glasso glasso
+#' @import CVXR
+#' @export
+#'
+lglasso=function(data,lambda, type=c("general","expFixed","twoPara"),expFix=1,group=NULL,maxit=100,
+                 tol=10^(-4),lower=c(0.01,0.1),upper=c(10,5),
+                 ...)
+
+  {
+if (is.null(group) & length(lambda)!=1 | !is.null(group) & length(lambda) !=2) {
+  stop("Arguments (group, lambda) do not match!")
+}
+  type=match.arg(type)
+  if (type=="general"){
+    m3=length(unique(data[,2]))
+    p=ncol(data)-2
+    A=diag(m3)
+    B=diag(p)
+    k=0
+
+    while(1){
+      k=k+1
+      # if (length(lambda)==1){
+
+      A1=AA(data = data,B = B, type=type,...)$corMatrix
+      B1=BB(data=data,A=A,lambda = lambda, type=type)$preMatrix
+
+      d1=round(max(abs(B-B1)),3)
+      d2=round(max(abs(A-A1)),3)
+      print(paste0("iteration ",k, " precision difference: ",d1 , " /correlation difference: ",d2))
+      if (d1<=tol & d2<= tol ){
+        if (is.null(group)){
+          return(list(preMatrix=B1, corMatrix=A1))
+          }else{
+          individial=heternetwork(data = data,lambda = lambda[2],homo = B1, group=group)
+          return(list(preMatrix=B1, corMatrix=A1, individial=individial))
+        }
+
+      }else{
+        A=A1
+        B=B1
+      }
+      if (k>=maxit){
+        warning("Algorithm did not converge!")
+
+        if (is.null(group)){
+          return(list(preMatrix=B1, corMatrix=A1))
+        }else{
+          individial=heternetwork(data = data,lambda = lambda[2],homo = B1, group=group)
+          return(list(preMatrix=B1, corMatrix=A1, individial=individial))
+        }
+
+      }
+    }
+  }
+
+  if (type == "expFixed"){
+    if (is.null(expFix) | length(expFix)!=1 | !is.numeric(expFix)){stop("Argument expFix is not correctly specified!")}
+    p=ncol(data)-2
+    fre=table(data$subject)
+    A=vector("list",length(fre))
+    for (i in 1:length(A)) {
+      A[[i]]=diag(fre[i])
+    }
+    B=diag(p)
+    k=0
+tau0=control$tau0
+    while(1){
+      k=k+1
+
+      #A1=AA(data = data,B = B, type=type,fix=expFix, init=tau0,lower = lower,upper = upper)
+      A1=AA(data = data,B = B, type=type,expFix,...)
+      B1=BB(data=data,A=A,lambda = lambda[1], type=type)$preMatrix
+
+      d1=round(max(abs(B-B1)),4)
+      d2=round(max(abs(tau0-A1$tau)),4)
+      print(paste0("iteration ",k, " precision difference: ",d1 , " /correlation tau difference: ",d2))
+      if (d1<=tol & d2<= tol ){
+#browser()
+        if (is.null(group)){
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0))
+        }else{
+
+          individial=heternetwork(data = data,lambda = lambda[2],homo = B1, group=group)
+
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0, individual=individial))
+        }
+
+
+
+      }else{
+        A=A1$corMatrixList
+        B=B1
+        tau0=A1$tau
+      }
+      if (k>=maxit){
+        warning("Algorithm did not converge!")
+        if (is.null(group)){
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0))
+        }else{
+          #browser()
+          individial=heternetwork(data = data,lambda = lambda[2],homo = B1, group=group)
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0, individual=individial))
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+  if (type == "twoPara"){
+    p=ncol(data)-2
+    fre=table(data$subject)
+    A=vector("list",length(fre))
+    for (i in 1:length(A)) {
+      A[[i]]=diag(fre[i])
+    }
+    B=diag(p)
+    k=0
+tau0=control$tau0
+    while(1){
+      k=k+1
+
+      A1=AA(data = data,B = B, type=type, fix=fix)
+      B1=BB(data=data,A=A,lambda = lambda[1], type=type)$preMatrix
+
+      d1=round(max(abs(B-B1)),4)
+      d2=round(max(abs(tau0-A1$tau)),4)
+      print(paste0("iteration ",k, " precision difference: ",d1 , " /correlation tau difference: ",d2))
+      if (d1<= tol & d2<= tol ){
+
+        if (is.null(group)){
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0))
+        }else{
+
+          individial=heternetwork(data = data,lambda = lambda[2],homo = B1, group=group)
+
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0, individual=individial))
+        }
+
+      }else{
+        A=A1$corMatrixList
+        B=B1
+        tau0=A1$tau
+      }
+      if (k>= maxit){
+        warning("Algorithm did not converge!")
+
+        if (is.null(group)){
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0))
+        }else{
+          #browser()
+          individial=heternetwork(data = data,lambda = lambda[2],homo = B1, group=group)
+          return(list(preMatrix=B1, corMatrixList=A1$corMatrixList, tauhat=tau0, individual=individial))
+      }
+    }
+    }
+
+  }
+}
+
+
+
+
+
+heternetwork=function(data,lambda,homo, group){
+  if (length(group)!=nrow(data)){
+    stop("Argument group doens not match data!")
+    }
+  m3=length(unique(group))
+  data_list=split(data,group)
+  p=ncol(data)-2
+ S=vector("list",m3)
+ Q=vector("list",m3)
+  obj=0
+  aa=0
+for (i in 1:m3) {
+  S[[i]]= Variable(p,p,PSD=TRUE)
+  Q[[i]]=cov(data_list[[i]][,-c(1,2)])
+  obj=obj+log_det(S[[i]])-matrix_trace(S[[i]]%*%Q[[i]])
+  aa=aa+ sum(abs(S[[i]]-homo))
+}
+constr=list(aa<=1/lambda)
+
+prob=Problem(Maximize(obj),constr)
+result=CVXR::solve(prob,solver="SCS")
+S_est= lapply(S, function(x) result$getValue(x))
+return(individual=S_est)
+}
+
+
+llike=function(data,A, preMatrix, individual){
+
+
+
+}
+
