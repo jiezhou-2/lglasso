08caf0e9287488b485ac9ad1b7afbabdfdfc46af
Jie Zhou <chowstat@gmail.com>
2026-09-09 14:10:29 -0400

    clean the house

--- data-raw/DATASET.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-
-#usethis::use_data(sample_data, overwrite = TRUE,compress = "xz")
-
-tau0=c(0.8,1)
-
-usethis::use_data(tau0, internal = TRUE, overwrite = TRUE)
